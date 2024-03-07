c742bdaa4d6fa5302c42744e4d258a6bdeca8cb8
worada.sir <worada.sir@axonstech.com>
2024-03-07 11:51:59 +0700

    Queries: Component State in React

--- job-board/client/src/pages/HomePage.js
+++ job-board/client/src/pages/HomePage.js
@@ -1,10 +1,18 @@
 import JobList from "../components/JobList";
-import { jobs } from "../lib/fake-data";
+// import { jobs } from "../lib/fake-data";
+import { useEffect, useState } from "react";
 import { getJobs } from "../lib/graphql/queries";
 
 getJobs().then((jobs) => console.log("jobs:", jobs));
 
 function HomePage() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then(setJobs);
+  }, []);
+
+  console.log("[HomePage] jobs:", jobs);
+
   return (
     <div>
       <h1 className="title">Job Board</h1>
