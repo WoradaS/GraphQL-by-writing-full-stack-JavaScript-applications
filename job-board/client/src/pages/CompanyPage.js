0921e1652c257e915df829cfb100c9372f3f342e
worada.sir <worada.sir@axonstech.com>
2024-03-07 12:03:55 +0700

    Queries: Company by ID

--- job-board/client/src/pages/CompanyPage.js
+++ job-board/client/src/pages/CompanyPage.js
@@ -1,10 +1,23 @@
 import { useParams } from "react-router";
-import { companies } from "../lib/fake-data";
+// import { companies } from "../lib/fake-data";
+import { useEffect, useState } from "react";
+import { getCompany } from "../lib/graphql/queries";
 
 function CompanyPage() {
   const { companyId } = useParams();
 
-  const company = companies.find((company) => company.id === companyId);
+  const [company, setCompany] = useState();
+
+  useEffect(() => {
+    getCompany(companyId).then(setCompany);
+  }, [companyId]);
+
+  console.log("[CompanyPage] company:", company);
+
+  if (!company) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1 className="title">{company.name}</h1>
